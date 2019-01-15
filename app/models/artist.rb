d88a92f3897a459ef0519999d083c7ceae45033e
github username <github email address>
2019-01-15 18:57:45 -0500

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,59 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.total_expirience
+    i = 0
+    @@all.each do |artist|
+      i += artist.years_experience
+    end
+    i
+  end
+
+  def self.most_prolific
+    @@all.max_by do |artist|
+      artist.paintings.length
+    end
+  end
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
 
-end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |my_painting|
+      my_painting.gallery
+    end
+  end
+
+  def cities
+    paintings.map do |my_painting|
+      my_painting.gallery.city
+    end
+  end
+
+  def create_painting(title, price, gallery) #(title, price, artist, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
+
+
+
+
+end #end of Artist class
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,42 @@
 class Gallery
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+
+    @@all << self
   end
 
-end
+  def paintings
+    Painting.all.select do |paintings|
+      paintings.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |my_paintings|
+      my_paintings.artist
+    end
+  end
+
+  def artist_names
+    paintings.map do |my_paintings|
+      my_paintings.artist.name
+    end
+  end
+
+  def most_expensive_painting
+    self.paintings.max_by do |paintings|
+      paintings.price
+    end
+  end
+
+end #end of gallery class
