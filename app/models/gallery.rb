4de0887e7bd1deabb225c3f95c6c2d9bb41d1fc4
Linya Hu <lh62594@yahoo.com>
2019-01-15 18:04:50 -0500

    finished version

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,37 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
+  ########### CLASS METHODS ###########
+  def self.all
+    @@all
+  end
+
+  ########### INSTANCE METHODS ###########
   def initialize(name, city)
     @name = name
     @city = city
+
+    @@all << self
   end
 
-end
+  def paintings
+    Painting.all.select{|painting| painting.gallery == self}
+  end
+
+  def artists
+    self.paintings.map{|painting| painting.artist}.uniq
+  end
+
+  def artist_names
+    self.artists.map{|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.max do |a, b|
+      a.price <=> b.price
+    end
+  end
+
+end #end of Gallery class
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,26 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
-    @title = title
-    @price = price
+  ########### CLASS METHODS ###########
+  def self.all
+    @@all
   end
 
-end
+  def self.total_price
+    self.all.map{|painting| painting.price}.reduce(:+)
+  end
+
+  ########### INSTANCE METHODS ###########
+  def initialize(title, price, artist, gallery)
+    @title = title
+    @price = price
+    @artist = artist
+    @gallery = gallery
+
+    @@all << self
+  end
+
+end #end of Painting class
