94e8b4cb6266e4d08eb07ec88364e16a176dac27
github username <github email address>
2019-01-15 00:04:26 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,40 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|art| art.artist == self}
+  end
+
+  def galleries
+    paintings.map {|venue| venue.gallary.name}.uniq
+  end
+
+  def cities
+    paintings.map {|venue| venue.gallary.city}.uniq
+  end
+
+  def create_painting(title,price,gallary)
+    Painting.new(title,price,self,gallary)
+  end
+
+  def self.total_experience
+    self.all.inject(0) {|sum,exp| sum + exp.years_experience}
+  end
+
+  def self.most_prolific
+    self.all.max_by {|exp| exp.years_experience}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,19 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Leonardo",25)
+a2 = Artist.new("Michelangelo", 30)
+a3 = Artist.new("Raphael", 45)
+a4 = Artist.new("Donatello", 80)
 
+g1 = Gallery.new("Palace of Fine Arts", "San Fransico")
+g2 = Gallery.new("Versailles", "Paris")
+
+p1 = Painting.new("P1",500,a1,g1)
+p2 = Painting.new("P2",400,a2,g2)
+p3 = Painting.new("P3",300,a1,g2)
+p4 = Painting.new("P4",200,a1,g1)
+p5 = Painting.new("P5",100,a3,g1)
+p6 = Painting.new("P6",1000,a4,g1)
 
 
 binding.pry
