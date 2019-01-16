6a072d89af86c952355fb3eb9b1ed2e076dd3cd4
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-16 13:12:17 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,69 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.total_experience
+    exp_array = self.all.map do | artist |
+      artist.years_experience
+    end
+    sum = 0
+    exp_array.each do | exp |
+      sum += exp
+    end
+    sum
+  end
+
+  def self.most_prolific
+    paintings_hash = Hash.new(0)
+    @@all.each do | artist |
+      paintings_hash[artist] = artist.paintings.count.to_f
+    end
+
+    experience_hash = Hash.new(0)
+    @@all.each do | artist |
+      experience_hash[artist] = artist.years_experience.to_f
+    end
+
+    prolific_hash = Hash.new(0)
+    @@all.each do | artist |
+      prolific_hash[artist] = paintings_hash[artist] / experience_hash[artist]
+    end
+
+    result_array = prolific_hash.max_by { | k, v | v }
+    result_array[0]
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
+  def paintings
+    Painting.all.select do | painting |
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map do | painting |
+      painting.gallery
+    end
+  end
+
+  def cities
+    city_array = self.galleries.map do | gallery |
+      gallery.city
+    end
+    city_array.uniq
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
-    @title = title
-    @price = price
+  def self.all
+    @@all
   end
 
+  def self.total_price
+    price_array = self.all.map do | painting |
+      painting.price
+    end
+    sum = 0
+    total = price_array.each do | price |
+      sum += price
+    end
+    sum
+  end
+
+  def initialize(title, price, gallery, artist)
+    @title = title
+    @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
+  end
 end
